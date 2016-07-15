fb6ad2288f91ab99027c4dabe78eb13721c1e94d
Faylite <benvev95@gmail.com>
2016-07-15 07:07:28 +0200

    Setup angular mockup

---
+++ resources/assets/js/app.js
@@ -0,0 +1,25 @@
+(function() {
+	var app = angular.module('taskList', []);
+
+	app.controller('TasksController', function() {
+
+	}
+
+	var tasks = [
+		{ title: 'Task #1', description: 'Task description.....' },
+		{ title: 'Task #2', description: 'Task description.....' },
+		{ title: 'Task #3', description: 'Task description.....' },
+		{ title: 'Task #4', description: 'Task description.....' },
+		{ title: 'Task #5', description: 'Task description.....' },
+		{ title: 'Task #6', description: 'Task description.....' },
+		{ title: 'Task #7', description: 'Task description.....' },
+		{ title: 'Task #8', description: 'Task description.....' },
+		{ title: 'Task #9', description: 'Task description.....' },
+		{ title: 'Task #10', description: 'Task description.....' },
+		{ title: 'Task #11', description: 'Task description.....' },
+		{ title: 'Task #12', description: 'Task description.....' },
+		{ title: 'Task #13', description: 'Task description.....' },
+		{ title: 'Task #14', description: 'Task description.....' },
+		{ title: 'Task #15', description: 'Task description.....' }
+	];
+})();
