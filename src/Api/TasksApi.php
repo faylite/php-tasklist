c85a6b2ad417c30956262fd2ee08d23f194f8bec
Faylite <benvev95@gmail.com>
2016-07-16 18:04:10 +0200

    Added api request handlers

---
+++ src/Api/TasksApi.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * GetApi.php
+ *
+ * Used for classes that returns data to get requests
+ */
+namespace Faylite\TaskList\Api;
+
+class GetTasksApi implements ApiRequestHandler
+{
+
+}
