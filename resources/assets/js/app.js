ef0473c1454eafd18deab708e0011bbc1bbdd9cc
Faylite <benvev95@gmail.com>
2016-07-18 05:09:10 +0200

    Added functions to manipulate tasks

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,15 +1,32 @@
 var app = angular.module('taskList', []);
 
 app.controller('TasksController', function($scope, $http, $timeout) {
-	var updateList = function() {
+	// Gets the list of tasks from the server
+	$scope.updateList = function() {
 		$http({
 			method: 'GET', url: '/api/v1/tasks/'
 		}).then(function successCallback(response) {
-			$scope.tasks = response.data.tasks;
-
-			// Check for changes
-			$timeout(updateList, 5000);
+			$scope.tasks = response.data;
 		});
 	};
-	updateList();
+	// Update the list on load
+	$scope.updateList();
+	
+	// Deletes the task with the requested ID
+	$scope.deleteTask = function(taskID) {
+		Materialize.toast('Task deleted!', 4000);
+		$scope.updateList();
+	};
+	
+	// Updates a task with the new specified title and description
+	$scope.editTask = function(taskTitle, taskDescription) {
+		Materialize.toast('Task updated!', 4000);
+		$scope.updateList();
+	};
+	
+	// Marks a task as done
+	$scope.markDone = function(taskID) {
+		Materialize.toast('Task marked as done!', 4000);
+		$scope.updateList();
+	}
 });
