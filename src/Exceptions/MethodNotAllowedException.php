b87894cdca29594c32be4bb32799fb291dbf1eb1
Faylite <benvev95@gmail.com>
2016-07-21 07:05:01 +0200

    Added exception classes

---
+++ src/Exceptions/MethodNotAllowedException.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Faylite\TaskList\Exceptions;
+
+/**
+ * The exception that gets thrown when a route was not found
+ */
+class MethodNotAllowedException extends Exception
+{
+	
+}
